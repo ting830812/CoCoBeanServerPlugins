164893db1a06dcf873b9a27fed61d5944f1d81ae
Ting <ting830812@gmail.com>
2021-08-07 14:45:25 +0800

    SnowballPortalFreezeAddon auto remove snowball after 3 sec through nether portal

--- SnowballPortalFreezeAddon/src/com/Ting/SnowballPortalFreezeAddon/EventListener.java
+++ SnowballPortalFreezeAddon/src/com/Ting/SnowballPortalFreezeAddon/EventListener.java
@@ -1,19 +1,30 @@
 package com.Ting.SnowballPortalFreezeAddon;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPortalEvent;
 
 import com.Ting.SnowballPortalFreezeAddon.Main;
+import org.bukkit.plugin.Plugin;
 
 public class EventListener implements Listener {
 
     @EventHandler
     public void onJoin(EntityPortalEvent e) {
         EntityType entityType = e.getEntityType();
+        Entity entity = e.getEntity();
         if (entityType == EntityType.SNOWBALL){
             e.setCancelled(true);
+            Bukkit.getScheduler().runTaskLater(Main.getInstance(), new Runnable() {
+                @Override
+                public void run() {
+                    entity.remove();
+                }
+            }, 10L);
+
         }
 
     }
